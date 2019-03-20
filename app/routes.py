2795cf103b7a739226967f866d6cc8785c542312
diorgulescu <pittick@gmail.com>
2019-03-20 15:05:36 +0200

    Added login and devices

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,32 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
+@app.route('/index.html')
+@app.route('/index')
 def index():
     return render_template("index.html")
 
 @app.route('/devices')
 def devices():
-    return render_template("devices.html")
+    device_list = [
+            {
+                'owner' : {'username' : 'admin'},
+                'name' : 'raspberry-pi-v3-1',
+                'state' : 'running',
+                'deployed_image' : 'Focused Linux',
+                'arch' : 'arm'
+                },
+            {
+                'owner' : {'username' : 'admin'},
+                'name' : 'raspberry-pi-v2-server',
+                'state' : 'powered off',
+                'deployed_image' : 'Raspbian',
+                'arch' : 'arm'
+                }
+            ]
+    return render_template("devices.html", devices=device_list)
 
 @app.route('/lab')
 def lab_view():
@@ -17,9 +36,13 @@ def lab_view():
 def user_dashboard():
     return "A user landing page, displaying relevant info for each and every account"
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return "Login page/form"
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login requested for user {}, remember_me={}".format(form.username.data,form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template("login.html", title="Sign In", form=form) 
 
 @app.route('/administration')
 def admin_section():
